000e3fdf34537e84d2332209b86352e98b1f5423
domshyra <domshyra@gmail.com>
2024-03-22 23:24:08 -0700

    made more testable

--- Web/src/components/recommendations/Form.jsx
+++ Web/src/components/recommendations/Form.jsx
@@ -1,7 +1,7 @@
 import { Box, Button, Container, Grid, Typography } from "@mui/material";
 import { FormProvider, useForm } from "react-hook-form";
 import { recommendationsForm, recommendationsRoot } from "../../constants/routes";
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useGetRecommendationQuery, useUpsertRecommendationMutation } from "../../redux/services/playlistRecommendationApi";
 import { useNavigate, useParams } from "react-router-dom";
 
@@ -73,94 +73,6 @@ const RecommendationForm = ({ setToast }) => {
 		[upsertRecommendation, isCreateMode, setToast, navigate]
 	);
 
-	const songs = useMemo(() => {
-		const songRow = (index) => {
-			return (
-				<Grid container direction="row" spacing={2} key={index}>
-					<Grid item xs={6}>
-						<GenericTextItem
-							name={`suggestions[${index}].title`}
-							id={`suggestions[${index}].title`}
-							customControl={methods.control}
-							fullWidth
-							isLoading={isLoading}
-						/>
-					</Grid>
-					<Grid item xs={6}>
-						<GenericTextItem
-							name={`suggestions[${index}].artist`}
-							id={`suggestions[${index}].artist`}
-							customControl={methods.control}
-							fullWidth
-							isLoading={isLoading}
-						/>
-					</Grid>
-				</Grid>
-			);
-		};
-
-		const songSection = [];
-		for (let i = 0; i < songRows; i++) {
-			songSection.push(songRow(i));
-		}
-
-		return songSection;
-	}, [isLoading, methods.control, songRows]);
-
-	const addSongRowButton = useMemo(() => {
-		return (
-			<Grid item>
-				<Button
-					key="addSongRow"
-					id="add-song-row-btn"
-					type="button"
-					color="primary"
-					variant="contained"
-					size="small"
-					startIcon={<AddIcon />}
-					onClick={() => setSongRows(songRows + 1)}
-				>
-					Add Song Row
-				</Button>
-			</Grid>
-		);
-	}, [songRows]);
-
-	const removeSongRowButton = useMemo(() => {
-		return (
-			<Grid item>
-				<Button
-					key="addSongRow"
-					id="add-song-row-btn"
-					type="button"
-					color="primary"
-					variant="contained"
-					size="small"
-					startIcon={<RemoveIcon />}
-					onClick={() => setSongRows(songRows - 1)}
-				>
-					Remove Song Row
-				</Button>
-			</Grid>
-		);
-	}, [songRows]);
-
-	const submitButton = useMemo(() => {
-		return (
-			<LoadingButton
-				key="bcegsSubmit"
-				id="submit-form-btn"
-				type="submit"
-				color="primary"
-				variant="contained"
-				loading={showLoadingButton}
-				disabled={!isDirty || !isValid}
-			>
-				{isCreateMode ? "Create" : "Save"}
-			</LoadingButton>
-		);
-	}, [isCreateMode, isDirty, isValid, showLoadingButton]);
-
 	return (
 		<Container>
 			<FormProvider key="fireStationForm" {...methods}>
@@ -169,28 +81,7 @@ const RecommendationForm = ({ setToast }) => {
 				</Typography>
 				<Box key="bcegsFormBox" component="form" noValidate autoComplete="off" onSubmit={methods.handleSubmit(onFormSubmit)}>
 					<Grid container direction="row" spacing={2}>
-						<Grid item xs={6}>
-							<GenericTextItem
-								name="name"
-								label="Name"
-								id="name"
-								rules={{ required: "Name is required." }}
-								customControl={methods.control}
-								isLoading={isLoading}
-							/>
-						</Grid>
-						<Grid item xs={6}>
-							<SelectItem
-								name="genre"
-								label="Genre"
-								id="genre"
-								showLabel
-								rules={{ required: "Genre is required." }}
-								customControl={methods.control}
-								isLoading={genresAreLoading}
-								options={genres}
-							/>
-						</Grid>
+						<RequiredFields genres={genres} genresAreLoading={genresAreLoading} isLoading={isLoading} control={methods.control} />
 					</Grid>
 					<Grid container direction="row" spacing={2} py={1}>
 						<Grid item xs={12}>
@@ -209,8 +100,8 @@ const RecommendationForm = ({ setToast }) => {
 						Songs
 					</Typography>
 					<Grid container direction="row" justifyContent={"center"} spacing={2}>
-						{addSongRowButton}
-						{removeSongRowButton}
+						<AddSongRowButton setSongRows={setSongRows} songRows={songRows} />
+						<RemoveSongRowButton setSongRows={setSongRows} songRows={songRows} />
 					</Grid>
 					<Grid container direction="row" py={2} px={2}>
 						<Grid item xs={6}>
@@ -219,10 +110,10 @@ const RecommendationForm = ({ setToast }) => {
 						<Grid item xs={6}>
 							<Typography variant="h6">Artist</Typography>
 						</Grid>
-						{songs}
+						<Songs isLoading={isLoading} control={methods.control} songRows={songRows} />
 					</Grid>
 					<Grid container direction="row" mt={0} py={2}>
-						{submitButton}
+						<SubmitButton isCreateMode={isCreateMode} isDirty={isDirty} isValid={isValid} showLoadingButton={showLoadingButton} />
 					</Grid>
 					<DevTool control={methods.control} />
 				</Box>
@@ -230,6 +121,158 @@ const RecommendationForm = ({ setToast }) => {
 		</Container>
 	);
 };
+
+/**
+ * Memoized JSX element representing the required fields in the form.
+ * Both Name and Genre are required fields.
+ * @type {JSX.Element}
+ */
+const RequiredFields = ({ genres, genresAreLoading, isLoading, control }) => {
+	return (
+		<>
+			<Grid item xs={6}>
+				<GenericTextItem
+					name="name"
+					label="Name"
+					id="name"
+					rules={{ required: "Name is required." }}
+					customControl={control}
+					isLoading={isLoading}
+				/>
+			</Grid>
+			<Grid item xs={6}>
+				<SelectItem
+					name="genre"
+					label="Genre"
+					id="genre"
+					showLabel
+					rules={{ required: "Genre is required." }}
+					customControl={control}
+					isLoading={genresAreLoading}
+					options={genres}
+				/>
+			</Grid>
+		</>
+	);
+};
+
+/**
+ * Represents a list of songs.
+ * @type {React.ReactNode}
+ */
+const Songs = ({ isLoading, control, songRows }) => {
+	const songRow = (index) => {
+		return (
+			<Grid container direction="row" spacing={2} key={index}>
+				<Grid item xs={6}>
+					<GenericTextItem
+						name={`suggestions[${index}].title`}
+						id={`suggestions[${index}].title`}
+						customControl={control}
+						fullWidth
+						isLoading={isLoading}
+					/>
+				</Grid>
+				<Grid item xs={6}>
+					<GenericTextItem
+						name={`suggestions[${index}].artist`}
+						id={`suggestions[${index}].artist`}
+						customControl={control}
+						fullWidth
+						isLoading={isLoading}
+					/>
+				</Grid>
+			</Grid>
+		);
+	};
+
+	const songSection = [];
+	for (let i = 0; i < songRows; i++) {
+		songSection.push(songRow(i));
+	}
+
+	return songSection;
+};
+
+/**
+ * The submit button component.
+ *
+ * @returns {JSX.Element} The submit button JSX element.
+ */
+const SubmitButton = ({ isCreateMode, isDirty, isValid, showLoadingButton }) => {
+	const btnText = () => {
+		if (isCreateMode) {
+			return "Create";
+		}
+		return "Save";
+	};
+	const btnDisabled = () => {
+		return !isDirty || !isValid;
+	};
+
+	return (
+		<LoadingButton
+			key="bcegsSubmit"
+			id="submit-form-btn"
+			type="submit"
+			color="primary"
+			variant="contained"
+			loading={showLoadingButton}
+			disabled={btnDisabled()}
+		>
+			{btnText()}
+		</LoadingButton>
+	);
+};
+
+/**
+ * Button component for adding a song row.
+ *
+ * @returns {JSX.Element} The add song row button.
+ */
+const AddSongRowButton = ({ setSongRows, songRows }) => {
+	return (
+		<Grid item>
+			<Button
+				key="addSongRow"
+				id="add-song-row-btn"
+				type="button"
+				color="primary"
+				variant="contained"
+				size="small"
+				startIcon={<AddIcon />}
+				onClick={() => setSongRows(songRows + 1)}
+			>
+				Add Song Row
+			</Button>
+		</Grid>
+	);
+};
+
+/**
+ * Button component for removing a song row.
+ *
+ * @type {React.ReactNode}
+ */
+const RemoveSongRowButton = ({ setSongRows, songRows }) => {
+	return (
+		<Grid item>
+			<Button
+				key="addSongRow"
+				id="add-song-row-btn"
+				type="button"
+				color="primary"
+				variant="contained"
+				size="small"
+				startIcon={<RemoveIcon />}
+				onClick={() => setSongRows(songRows - 1)}
+			>
+				Remove Song Row
+			</Button>
+		</Grid>
+	);
+};
+
 
 function mapStateToProps() {
 	return {};
